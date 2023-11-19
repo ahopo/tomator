4193c215dd5bf33d21fde792fc54a16d73904676
AxOPO <143918312+AxOPO@users.noreply.github.com>
2023-11-19 20:20:51 +0800

    modify convert config print output

--- cmd/convert-config/main.go
+++ cmd/convert-config/main.go
@@ -1,6 +1,7 @@
 package yjjy
 
 import (
+	"fmt"
 	"log"
 	"tomator/cmd/common/util"
 
@@ -15,18 +16,21 @@ func model() Model {
 	}
 }
 func (m *Model) convertYJ(cCtx *cli.Context) error {
-	file := util.InitFile(cCtx.String("file"))
+	//STRING INPUT
 	str := cCtx.String("string")
+	//check if str flag has value then process.
+	if len(str) > 0 {
+		y, _ := yaml.YAMLToJSON([]byte(str))
+		fmt.Println(string(y))
+		return nil
+	}
+	//FILE INPUT
+	file := util.InitFile(cCtx.String("file"))
 	//validate file exist and in yaml extension.
 	if file.IsValid && file.Ext != m.YAML {
 		log.Fatal("Not a yaml file.")
 	}
-	//check if str flag has value then process.
-	if len(str) > 0 {
-		y, _ := yaml.YAMLToJSON([]byte(str))
-		log.Println(string(y))
-		return nil
-	}
+
 	//covert file content and create new file.
 	y, _ := yaml.YAMLToJSON([]byte(file.Content))
 	newFile := file.CreateUsingExistingName(file.ExtType.JSON)
@@ -36,19 +40,21 @@ func (m *Model) convertYJ(cCtx *cli.Context) error {
 	return nil
 }
 func (m *Model) convertJY(cCtx *cli.Context) error {
-
-	file := util.InitFile(cCtx.String("file"))
+	//STRING INPUT
 	str := cCtx.String("string")
+	//check if str flag has value then process.
+	if len(str) > 0 {
+		y, _ := yaml.JSONToYAML([]byte(str))
+		fmt.Println(string(y))
+		return nil
+	}
+	//FILE INTPU
+	file := util.InitFile(cCtx.String("file"))
 	//validate file exist and in yaml extension.
 	if file.IsValid && file.Ext != m.JSON {
 		log.Fatal("Not a yaml file.")
 	}
-	//check if str flag has value then process.
-	if len(str) > 0 {
-		y, _ := yaml.YAMLToJSON([]byte(str))
-		log.Println(string(y))
-		return nil
-	}
+
 	//covert file content and create new file.
 	y, _ := yaml.JSONToYAML([]byte(file.Content))
 	newFile := file.CreateUsingExistingName(file.ExtType.JSON)
